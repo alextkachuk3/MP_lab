56c2cd8ffa33f2a10bd60ba412b2cd5d97aa61ce
alextkachuk3 <tkachuk.alexey@unicyb.kiev.ua>
2023-03-27 21:43:29 +0300

    Road class

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 
+from road import Road
+
 window_width = 1000
 window_height = 600
 
@@ -7,19 +9,8 @@ pygame.init()
 screen = pygame.display.set_mode((window_width, window_height))
 pygame.display.set_caption('Вжух вжух')
 
-road_lines_count = 7
-road_line_height_percent = 0.04
-line_width = window_width / road_lines_count
-line_height = window_height * road_line_height_percent
-line_begin_pos_x_coefficient = 5
-space_between_x_coefficient = 0.125
-line_pos_x = -window_width / line_begin_pos_x_coefficient + window_width / road_lines_count
-
-for i in range(0, road_lines_count):
-    line = pygame.Surface((line_width, line_height))
-    line.fill('White')
-    screen.blit(line, (line_pos_x, window_height * 0.5 - line_height * 0.5))
-    line_pos_x = line_pos_x + line_width + window_width / road_lines_count * space_between_x_coefficient
+road = Road(window_width, window_height, 7)
+road.blit(screen)
 
 running = True
 
---
+++ road.py
@@ -0,0 +1,25 @@
+import pygame
+
+
+class Road:
+    def __init__(self, window_width, window_height, lines_count):
+        road_line_height_percent = 0.04
+        line_begin_pos_x_coefficient = 5
+        space_between_x_coefficient = 1 / lines_count
+        self.line_width = window_width / lines_count
+        self.line_height = window_height * road_line_height_percent
+        self.line_pos_x = -window_width / 5 + window_width / lines_count
+
+        cur_line_pos_x = -window_width / line_begin_pos_x_coefficient + window_width / lines_count
+        self.line_pos_y = window_height * 0.5 - self.line_height * 0.5
+        self.lines_pos_x = []
+
+        for i in range(0, lines_count):
+            self.lines_pos_x.append(cur_line_pos_x)
+            cur_line_pos_x = cur_line_pos_x + self.line_width + window_width / lines_count * space_between_x_coefficient
+
+    def blit(self, surface):
+        for line_pos_x in self.lines_pos_x:
+            line = pygame.Surface((self.line_width, self.line_height))
+            line.fill('White')
+            surface.blit(line, (line_pos_x, self.line_pos_y))
