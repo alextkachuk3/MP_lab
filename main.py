1f1ba44f94c88a99a6d0bf337ab123da13994576
alextkachuk3 <tkachuk.alexey@unicyb.kiev.ua>
2023-03-27 22:41:59 +0300

    Basic car

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import os
 
 from road import Road
 
@@ -12,9 +13,14 @@ pygame.display.set_caption('Вжух вжух')
 road = Road(window_width, window_height, 7)
 road.blit(screen)
 
+picture = pygame.image.load(os.path.join('images', 'car.png'))
+picture = pygame.transform.smoothscale(picture, (300, 150))
+
+
 running = True
 
 while running:
+    screen.blit(picture, (100, 375))
     pygame.display.update()
 
     for event in pygame.event.get():
